5473bdfba29071772aa325e934699bed087bf106
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-10-27 16:09:33 -0600

    Initial Commit

---
+++ Large reducer/Large_reducerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Large_reducerApp.swift
+//  Large reducer
+//
+//  Created by Adam Garrett-Harris on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Large_reducerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
